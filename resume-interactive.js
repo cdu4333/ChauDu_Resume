2bdffefa0d5c6323b1dc7fa532766b88f7b28701
Chau Du <Hanhchau69@yahoo.com>
2020-11-15 10:54:47 -0600

    Add files via upload

---
+++ resume-interactive.js
@@ -0,0 +1,8 @@
+const myPhone = () => {
+    document.getElementById("myPhone").innerHTML="(480)285-4333"
+}
+
+const myProfilePic = () => {
+    document.getElementById("myProfilePic").style.display="none"
+}
+
